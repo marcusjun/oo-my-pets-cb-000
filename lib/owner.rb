df5cae299f023cda41d351a282369c241283f09f
Marcus Jun <mjun@calstate.edu>
2018-04-21 20:41:23 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,14 +50,12 @@ class Owner
 
   def buy_cat(name)
     new_cat=Cat.new(name)
-    @pets[:cats]<<new_cat  #@pets[cats]<<new_cat
-    #@pets[:cats].push(new_cat)
-
+    @pets[:cats]<<new_cat  
   end
 
   def buy_dog(name)
     new_dog=Dog.new(name)
-    @pets[:dogs]<<new_dog  #@pets[dogs]<<new_dog
+    @pets[:dogs]<<new_dog 
   end
 
   def buy_fish(name)
