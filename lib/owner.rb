c0afd3e9334d1380b7bba1a9c1c1aea4f1fbfe73
Marcus Jun <mjun@calstate.edu>
2018-04-21 20:46:55 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,69 +5,62 @@ class Owner
   attr_accessor :pets, :name
   attr_reader :species
 
+  #Some methods described in owner_spec.rb
+  #including #species and #name
+  #are covered by the above attr_accessor
+  #and/or attr_reader
+
   @@all=[]
 
   def self.all
     @@all
   end
 
-  def initialize(species) #="human")
+  def initialize(species)
     @species=species
-    #@name=name
 
     @pets= {fishes: [], cats: [], dogs: []}
 
-    #self.all.class<<self
     @@all<<self
+
+    #The following lines of code don't work
+    #self.all<<self
+    #self.all.class<<self
   end
 
   def self.count
-    @@all.length
-    #self.class.all.length   #self.all.length
+    #Either line of code works
+    #@@all.length
+    self.all.length
   end
 
   def self.reset_all
-    @@all.clear
-    #self.class.all.clear   #self.all.clear
+    #Either line of code works
+    #@@all.clear
+    self.all.clear
   end
-
-  #def species(species)
-    ##new_owner=self.new(name)
-    #@species=species
-  #end
 
   def say_species
     "I am a #{@species}."
+
+    #Don't use puts
     #puts "I am a #{@species}."
   end
 
-  #def pets
-    #@pets= {fishes: [], cats: [], dogs: []}
-  #end
 
   def buy_cat(name)
     new_cat=Cat.new(name)
-    @pets[:cats]<<new_cat  #@pets[cats]<<new_cat
-    #@pets[:cats].push(new_cat)
-    
+    @pets[:cats]<<new_cat
   end
 
   def buy_dog(name)
     new_dog=Dog.new(name)
-    @pets[:dogs]<<new_dog  #@pets[dogs]<<new_dog
+    @pets[:dogs]<<new_dog
   end
 
   def buy_fish(name)
     new_fish=Fish.new(name)
-    @pets[:fishes]<<new_fish  #@pets[fishes]<<new_fish
-
-    #self.pets[:fishes]<<new_fish
-
-    #@pets[:fishes]=new_fish
-
-    #binding.pry
-
-    ##@pets[:fishes]<<Fish.new(name)
+    @pets[:fishes]<<new_fish
   end
 
   def walk_dogs
@@ -96,24 +89,26 @@ class Owner
     #@pets[:cats].mood="nervous"
     #@pets[:fishes].mood="nervous"
 
-    @pets.each do |animal, animal_array|
-      animal_array.clear
-    end
 
-    #@pets[:dogs].each {|animal_array| animal_array.clear}
-    #@pets[:cats].each {|animal_array| animal_array.clear}
-    #@pets[:fishes].each {|animal_array| animal_array.clear}
+    @pets.each {|animal, animal_array| animal_array.clear}
+
+    #This works too.
+    #@pets.each do |animal, animal_array|
+      #animal_array.clear
+    #end
 
   end
 
   def list_pets
-    number_fish=@pets[:fishes].length
-    number_dogs=@pets[:dogs].length
-    number_cats=@pets[:cats].length
+    #number_fish=@pets[:fishes].length
+    #number_dogs=@pets[:dogs].length
+    #number_cats=@pets[:cats].length
 
-    "I have #{number_fish} fish, #{number_dogs} dog(s), and #{number_cats} cat(s)."
-    #puts "I have #{number_fish} fish, #{number_dogs} dog(s), and #{number_cats} cat(s)."
+    #don't use puts
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+    #"I have #{number_fish} fish, #{number_dogs} dog(s), and #{number_cats} cat(s)."
 
+    #Alternative, wordy approach. don't know if it works
     #number_fish=0
     #number_dogs=0
     #number_cats=0
